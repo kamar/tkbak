67941a5814e9469f640c9f99a11340b4178387c2
kamar <kamar@localhost>
2013-11-19 17:46:05 +0200

    added backup/docs/AUTHORS added backup/docs/TRANSLATORS added backup/docs/VERSION added backup/docs/gpl-3.0.txt added backup/docs/gplv3-127x51.gif added backup/docs/gplv3-127x51.png added backup/docs/gplv3-88x31.gif added backup/docs/gplv3-88x31.png added backup/docs/tkbackup.gif added backup/docs/tkbackup.png added backup/locale/el_GR/LC_MESSAGES/tkbackup.mo added backup/tkbackup.py added tkbak.py changed MANIFEST.in changed setup.py removed AUTHORS removed TRANSLATORS removed VERSION removed backup/__init__.py removed docs/gpl-3.0.txt removed docs/gplv3-127x51.gif removed docs/gplv3-127x51.png removed docs/gplv3-88x31.gif removed docs/gplv3-88x31.png removed docs/tkbackup.gif removed docs/tkbackup.png removed locale/el_GR/LC_MESSAGES/tkbackup.mo

--- setup.py
+++ setup.py
@@ -7,20 +7,23 @@ Created on 18 Νοε 2013
 '''
 from distutils.core import setup
 
+files = ['docs/*', 'locale/el_GR/LC_MESSAGES/*']
+
 setup(name='tkbackup',
-      version=open('VERSION').read().strip(),
+      version=open('backup/docs/VERSION').read().strip(),
       description='Simple Backup Program',
       author='Konstas Marmatakis',
       author_email='marmako@gmail.com',
       url='https://bitbucket.org/kamar/tkbackup',
       packages=['backup'],
-      scripts=['tkbackup'],
+      scripts=['tkbak.py'],
+      package_data = {'backup': files},
       license='GNU/GPLv3',
-      data_files=[('docs', ['docs/gpl-3.0.txt', 'AUTHORS', 'README.rst', 'TRANSLATORS', 'VERSION']),
-                  ('images', ['docs/gplv3-127x51.gif', 'docs/gplv3-127x51.png',
-                              'docs/gplv3-88x31.gif', 'docs/gplv3-88x31.png', 
-                              'docs/tkbackup.gif', 'docs/tkbackup.png'])],
-      long_description=open('README.rst').read(),
+#       data_files=[('docs', ['docs/gpl-3.0.txt', 'AUTHORS', 'README.rst', 'TRANSLATORS', 'VERSION']),
+#                   ('images', ['docs/gplv3-127x51.gif', 'docs/gplv3-127x51.png',
+#                               'docs/gplv3-88x31.gif', 'docs/gplv3-88x31.png', 
+#                               'docs/tkbackup.gif', 'docs/tkbackup.png'])],
+      long_description=open('backup/docs/README.rst').read(),
       classifiers=['Development Status :: 4 - Beta',
                    'Environment :: X11 Applications',
                    'Intended Audience :: Other Audience',
