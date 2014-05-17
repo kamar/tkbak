a6c189e2ed6afd04aecd958cd9caff0c1592f409
Konstas Marmatakis <marmako@gmail.com>
2014-05-17 21:26:39 +0300

    Minor changes.

--- backup/checkversion.py
+++ backup/checkversion.py
@@ -1,3 +1,26 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+#==================================================================================
+#  Copyright:
+#
+#      Copyright (C) 2013 Konstas Marmatakis <marmako@gmail.com>
+#
+#   License:
+#
+#      This program is free software; you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License version 3 as
+#      published by the Free Software Foundation.
+#
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#
+#      You should have received a copy of the GNU General Public License
+#      along with this package; if not, write to the Free Software
+#      Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+#===============================================================
+
 import re
 import os
 import sys
