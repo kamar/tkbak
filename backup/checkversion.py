a9f8de23fa681a6036abd7be44f9a66c661e4bf9
kamar <kamar@localhost>
2014-05-22 23:47:23 +0300

    Minor changes.

--- backup/checkversion.py
+++ backup/checkversion.py
@@ -24,8 +24,19 @@
 import re
 import os
 import sys
+import gettext
+import locale
 import urllib
 import urllib.request
+
+glossa = locale.getdefaultlocale()[0]
+
+dir_name = os.path.dirname(__file__)
+
+t = gettext.translation("tkbak", localedir=dir_name + os.sep + "locale", codeset='utf-8', fallback=True, \
+                        languages=[glossa])
+_ = t.gettext
+t.install()
 
 
 def check_version():
@@ -63,51 +74,57 @@ License: GNU/GPL v 3.0
         the_url = urllib.parse.unquote(the_url)
         site = urllib.request.urlopen(the_url)
         r = site.info()
-        #file_name = the_url.split('/)[-1:] 
+        #file_name = the_url.split('/)[-1:]
+        #print(len(site.read()))
         file_size = int(r.get('Content-Length'))
+#         site.seek(0)
+        
         file_name = the_url.split('/')[-1:][0] 
         file_seek = 0
-        sinexeia = False
+        
         if os.path.exists(file_name):
             localfile = open(file_name, 'rb')
             content = localfile.read()
             localfile.close()
+            
             if len(content) == file_size:
-                print('Το αρχείο υπάρχει ήδη.')
-                break
-            else:
-                sinexeia = True
+                print(_('The file allready exists.'))
+                return None
+        
 ##        print('Κατεβάζω το αρχείο {0}.\n'.format(os.path.abspath(localfile)))
-        print("Κατεβάζω: {0}\nBytes: {1} KBytes: {2:.2f}".format(os.path.abspath(file_name),\
+        print(_("Downloading: {0}\nBytes: {1} KBytes: {2:.2f}").format(os.path.abspath(file_name),\
                 file_size, int(file_size/1024)))
 
         try:
-            if sinexeia:
+            if file_size <= block_size:
                 fh = open(file_name, 'wb')
-                megethos_arxeiou_kt = len(content)
-                fh.write(content)
+                megethos_arxeiou_kt = file_size
+                fh.write(site.read())
             else:
                 fh = open(file_name, 'wb')
                 megethos_arxeiou_kt = 0
 
-            while True:
-                site.read(file_seek)
-                buffer = site.read(block_size)
-                if not buffer:
-                    break
-
-                megethos_arxeiou_kt += len(buffer)
-                fh.write(buffer)
-                prcnt = (megethos_arxeiou_kt / file_size)
-                
-                katastasi = "{0:>6,d}kb {1:.2%}".format(round(megethos_arxeiou_kt/1024),\
-                                                        prcnt)
-                katastasi = katastasi + " " + "=" * int(prcnt * 100 / 2) + ">"
-                sys.stdout.write("\rΚατέβηκαν: " + katastasi)
-                sys.stdout.flush()
-
-            sys.stdout.write("\n")
-
+                while True:
+#                     site.read(file_seek)
+                    buffer = site.read(block_size)
+                    if not buffer:
+                        break
+    
+                    megethos_arxeiou_kt += len(buffer)
+                    fh.write(buffer)
+                    
+                    prcnt = (megethos_arxeiou_kt / file_size)
+                    
+                    katastasi = "{0:>6,d}kb {1:.2%}".format(round(megethos_arxeiou_kt/1024),\
+                                                            prcnt)
+                    katastasi = katastasi + " " + "=" * int(prcnt * 100 / 2) + ">"
+                    print(_("Downloaded: {0}").format(katastasi))
+#                     sys.stdout.write("\rΚατέβηκαν: " + katastasi)
+#                     sys.stdout.flush()
+#  
+#             sys.stdout.write("\n")
+            print()
+            print(_("Downloading completed."))
         finally:
             fh.close()
 
@@ -116,14 +133,18 @@ def downloadthefile(ver):
     Downloads the newest version.
     """
     try:
-        download_me('https://testpypi.python.org/packages/source/t/tkbak/tkbak-{0}.tar.gz'.format(ver))
+        filen = 'https://testpypi.python.org/packages/source/t/tkbak/tkbak-{0}.tar.gz'.format(ver)
+        download_me(filen)
     except:
+        print(_("Cannot download: {0}").format(filen))
         try:
-            download_me('https://testpypi.python.org/packages/source/t/tkbak/tkbak-{0}.tar.gz'.format(ver))
+            filen  = 'https://testpypi.python.org/packages/source/t/tkbak/tkbak-{0}.zip'.format(ver)
+            print(_("Trying: {0}").format(filen))
+            download_me(filen)
         except:
-            print("Cannot download: {0}".format('https://testpypi.python.org/packages/source/t/tkbak/tkbak-{0}.tar.gz'.format(ver)))
+            print(_("Cannot download: {0}").format(filen))
              
 if __name__ == '__main__':
-    #download_me('https://testpypi.python.org/packages/source/t/tkbak/tkbak-{0}.tar.gz'.format(mobj.group(1)))
-    print(check_version())
+    download_me('https://testpypi.python.org/packages/source/t/tkbak/tkbak-{0}.tar.gz'.format(check_version()))
+    #print(check_version())
     
