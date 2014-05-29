a81fbae36c2a28fcce127acab68a7775c3d4ebdc
Konstas Marmatakis <marmako@gmail.com>
2014-05-29 12:29:33 +0300

    Added deleteme.sh

---
+++ deleteme.sh
@@ -0,0 +1,7 @@
+#!/urs/bin/env bash
+
+sudo rm -v /usr/bin/{tkbak, cr_shortcut.py}
+sudo rm -v /usr/local/bin/{tkbak,cr_shortcut.py}
+sudo rm -v /usr/share/applications/tkbak.desktop
+sudo rm -vr /usr/local/lib/python3.2/dist-packages/backup
+sudo rm -v /usr/local/lib/python3.2/dist-packages/tkbak-*.egg-info 
